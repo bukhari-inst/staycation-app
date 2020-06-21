e942feaffe928582e3c7f32687fd49eb7ead32a3
bukhori-inst <bukhori414647@gmail.com>
2020-06-21 18:56:20 +0700

    Menyusun component hero

---
+++ src/parts/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ImageHero from 'assets/images/img-hero.jpg';
+import ImageHero_ from 'assets/images/img-hero-frame.jpg';
+import IconCities from 'assets/images/icons/IconCities.svg';
+import IconTraveler from 'assets/images/icons/IconTraveler.svg';
+import IconTreasure from 'assets/images/icons/IconTreasure.svg';
+
+export default function Hero(props) {
+  return <div></div>;
+}
