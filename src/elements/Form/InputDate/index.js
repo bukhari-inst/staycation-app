ad887919e0887c5cafd84c99c0992ba8c038d979
bukhori-inst <bukhori414647@gmail.com>
2020-12-31 19:13:38 +0700

    component input number part 2

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -25,7 +25,53 @@ export default function Number(props) {
     }
   };
 
-  return <div></div>;
+  //   untuk tombol - dan + di halaman booking page
+  const minus = () => {
+    value > min &&
+      onChange({
+        target: {
+          name: name,
+          value: +value - 1,
+        },
+      });
+  };
+
+  const plus = () => {
+    value < max &&
+      onChange({
+        target: {
+          name: name,
+          value: +value + 1,
+        },
+      });
+  };
+
+  return (
+    <div className={['input-number mb-3', props.outerClassName].join(' ')}>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text minus" onClick={minus}>
+            -
+          </span>
+        </div>
+        <input
+          min={min}
+          max={max}
+          name={name}
+          pattern="[0-9]*"
+          className="form-control"
+          placeholder={placeholder ? placeholder : '0'}
+          value={String(InputValue)}
+          onChange={onChange}
+        />
+        <div className="input-group-append">
+          <span className="input-group-text plus" onClick={plus}>
+            +
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Number.defaultProps = {
