4dcea29a09674bb0f91ecddf8f15d7fa2385ad37
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-04-22 10:45:44 +0700

    Ngebug berminggu" gk sadar, Solved salah 1 huruf, sharusnya exact > axact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Example from 'pages/Example';
 import 'assets/scss/style.scss';
-import LandingPage from 'pages/Example';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/example" component={Example} />
       </Router>
     </div>
   );
